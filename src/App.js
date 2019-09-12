cde9f8fd01127c39f6094dbfaddc90835e6a51e8
Nikki Wu <nikkiwu9@gmail.com>
2019-09-12 16:35:29 -0700

    fix url link to portfolio

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
                 <Container className='p-0' fluid={true}>
                     <Navigation/>
                     <Switch>
-                        <Route path='/' exact
+                        <Route path='/portfolio' exact
                                render={() => <Home title={this.state.home.title} nameText={this.state.home.nameText}
                                                    jobText={this.state.home.jobText}
                                                    locationText={this.state.home.locationText}
