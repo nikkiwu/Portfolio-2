fa1cd740ebcd01ca4dd21f9889cabcc3b45ef004
Nikki Wu <nikkiwu9@gmail.com>
2019-09-12 15:50:34 -0700

    Updates

---
+++ precache-manifest.b55491985c51b21ced3a0dd6817ea90a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271e022d3fa87937ecb95b050dfa7bbe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3bed705f343f589c5fad",
+    "url": "/portfolio/static/css/2.fe520fb9.chunk.css"
+  },
+  {
+    "revision": "dffe18519c62ca8d8054",
+    "url": "/portfolio/static/css/main.9de195a4.chunk.css"
+  },
+  {
+    "revision": "3bed705f343f589c5fad",
+    "url": "/portfolio/static/js/2.c0d2f460.chunk.js"
+  },
+  {
+    "revision": "dffe18519c62ca8d8054",
+    "url": "/portfolio/static/js/main.b51d3e03.chunk.js"
+  },
+  {
+    "revision": "7160f7b8aadec5f04929",
+    "url": "/portfolio/static/js/runtime~main.bb9c7cad.js"
+  },
+  {
+    "revision": "9a0b29b6d9d3a9d3e74ce03bed375aa4",
+    "url": "/portfolio/static/media/HelveticaNeueBold.9a0b29b6.woff"
+  },
+  {
+    "revision": "212707c7122923a41b2c05e4e7cf8ddb",
+    "url": "/portfolio/static/media/HelveticaNeueLight.212707c7.woff"
+  }
+]);
