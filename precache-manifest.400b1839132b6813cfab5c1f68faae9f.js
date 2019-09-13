e518ef85572542ed3125159b702f1a057b7c95ad
Nikki Wu <nikkiwu9@gmail.com>
2019-09-12 17:09:39 -0700

    Updates

---
+++ precache-manifest.400b1839132b6813cfab5c1f68faae9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408bfef32cc8fec2123ccf1654c0a95c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bed705f343f589c5fad",
+    "url": "./static/css/2.fe520fb9.chunk.css"
+  },
+  {
+    "revision": "055172d41d58cfbe4ec9",
+    "url": "./static/css/main.16413b99.chunk.css"
+  },
+  {
+    "revision": "3bed705f343f589c5fad",
+    "url": "./static/js/2.c0d2f460.chunk.js"
+  },
+  {
+    "revision": "055172d41d58cfbe4ec9",
+    "url": "./static/js/main.f76dfc53.chunk.js"
+  },
+  {
+    "revision": "f30bc223e44fdc8afe6f",
+    "url": "./static/js/runtime~main.45de1a86.js"
+  },
+  {
+    "revision": "9a0b29b6d9d3a9d3e74ce03bed375aa4",
+    "url": "./static/media/HelveticaNeueBold.9a0b29b6.woff"
+  },
+  {
+    "revision": "212707c7122923a41b2c05e4e7cf8ddb",
+    "url": "./static/media/HelveticaNeueLight.212707c7.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b0207f4471c34c2ebd8914f927699e3.js"
+  "./precache-manifest.400b1839132b6813cfab5c1f68faae9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
