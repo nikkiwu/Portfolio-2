415133e9c63fc97ae6a6a044c3eca2f039b77466
Nikki Wu <nikkiwu9@gmail.com>
2019-09-12 17:32:00 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.93b64dfbb253f4a7bb442d581ac58ea6.js"
+  "/portfolio/precache-manifest.1505342f78ac40ff2e253e11fcb1fe48.js"
 );
 
 self.addEventListener('message', (event) => {
