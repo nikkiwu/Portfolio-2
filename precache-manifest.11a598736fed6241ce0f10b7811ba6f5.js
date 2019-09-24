2129b7272c7f5194a731b2185bf71f1217529718
Nikki Wu <nikkiwu9@gmail.com>
2019-09-24 16:27:18 -0700

    Updates

---
+++ precache-manifest.11a598736fed6241ce0f10b7811ba6f5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa3442bc191a7a2c7abd42dd0a1da12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6386a1349061172af6e2",
+    "url": "/portfolio/static/css/2.5a60c6f9.chunk.css"
+  },
+  {
+    "revision": "6d577711d8eb03a1c8be",
+    "url": "/portfolio/static/css/main.39b2b57d.chunk.css"
+  },
+  {
+    "revision": "6386a1349061172af6e2",
+    "url": "/portfolio/static/js/2.106a27d0.chunk.js"
+  },
+  {
+    "revision": "6d577711d8eb03a1c8be",
+    "url": "/portfolio/static/js/main.5e228736.chunk.js"
+  },
+  {
+    "revision": "7160f7b8aadec5f04929",
+    "url": "/portfolio/static/js/runtime~main.bb9c7cad.js"
+  },
+  {
+    "revision": "9a0b29b6d9d3a9d3e74ce03bed375aa4",
+    "url": "/portfolio/static/media/HelveticaNeueBold.9a0b29b6.woff"
+  },
+  {
+    "revision": "212707c7122923a41b2c05e4e7cf8ddb",
+    "url": "/portfolio/static/media/HelveticaNeueLight.212707c7.woff"
+  },
+  {
+    "revision": "d5abf2c2956c245702a342b64695e4a1",
+    "url": "/portfolio/static/media/Nikki Wu Resume.d5abf2c2.pdf"
+  },
+  {
+    "revision": "2dfea475dbbdeea0bb542867cc16ea8f",
+    "url": "/portfolio/static/media/behance.2dfea475.png"
+  },
+  {
+    "revision": "68bf34e10d8c1192975bbac25145a9ef",
+    "url": "/portfolio/static/media/bleachereport.68bf34e1.png"
+  },
+  {
+    "revision": "8785f5a0145d0ec729fa4c88c718684f",
+    "url": "/portfolio/static/media/bonapp.8785f5a0.png"
+  },
+  {
+    "revision": "160e43e7b635ca604313974d1a63c013",
+    "url": "/portfolio/static/media/home-page-image.160e43e7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8cab14b782c2e494c4ac75eabb5cf1d0.js"
+  "/portfolio/precache-manifest.11a598736fed6241ce0f10b7811ba6f5.js"
 );
 
 self.addEventListener('message', (event) => {
